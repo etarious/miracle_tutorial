92b5faf626153a83bc8706c21447542f3142a76e
Etarious <ojameruayeetaoghene@gmail.com>
2023-04-05 17:50:34 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,83 @@
+// // alert("This is working externaly!");
+
+// let firstname;
+// var lastname;
+// const age = 21;
+
+// firstname = 'Kunle';
+
+// firstname = 'Alex';
+
+// lastname = 'James';
+
+// lastname = 'John';
+
+// let varcha = 123;
+
+// let int = 5;
+
+// let bool = true;
+
+// let executed = true;
+
+// let float = 1.5;
+
+
+
+// if (executed) {
+//     alert("True and working");
+// }else{
+//     alert("Not true!");
+// }
+
+// // age = 31;
+
+// let sentence = `Hello ${firstname}, welcome to my blog!`;
+
+// console.log(typeof float);
+// alert(firstname);
+
+
+
+
+//DECISIONS AND LOOPS
+
+// let y = 5;
+// let x = 3;
+
+// let raining = true;
+// let sunny = true;
+
+// if (5 + 2 == 10){
+//     console.log("Oops! Not Correct!");
+// }else if (5 + 5 == 10){
+//     console.log("Hurray! Correct!");
+// }
+
+
+// ARRAYS
+
+// let fruits = 'Appple' 'Mango' 'Orange';
+
+
+let fruits = new Array('Apple', 'Mango', 'orange');
+
+// let fruits = ['Apple', 'Mango', 'Orange', 31, 21.5, {full: 'Austine'}];
+
+// console.log(fruits[fruits.length - 1]);
+
+
+
+// alert, prompt, confirm...
+
+// alert("Working");
+let conf = confirm("Do you want to be redirected?");
+// let firstname = prompt("Enter  your First Name:");
+// console.log(`Hello ${firstname}!`);
+
+if (conf) {
+    document.location.href = "https://www.w3school.com";
+}
+
+
+
